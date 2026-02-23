7592d79703deeee8380913f7ff0f0095a1f0cb30
vishal911m <vishal911m@gmail.com>
2026-02-23 23:52:41 +0530

    Restocking Cakes

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const redux = require('redux');
 const createStore = redux.createStore
 
 const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
 
 function orderCake(){
   return {
     type: CAKE_ORDERED,
-    quantity: 1
+    payload: 1
+  }
+}
+
+function restockCake(qty = 1){
+  return{
+    type: CAKE_RESTOCKED,
+    payload: qty
   }
 }
 
@@ -20,7 +28,13 @@ const reducer = (state = initialState, action) => {
   switch(action.type){
     case CAKE_ORDERED:
       return{
+        ...state,
         numOfCakes: state.numOfCakes -1 
+      }
+    case CAKE_RESTOCKED:
+      return{
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload
       }
     default:
       return state
@@ -35,5 +49,6 @@ const unsubscribe = store.subscribe(()=>console.log('Updated state', store.getSt
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+store.dispatch(restockCake(3));
 
 unsubscribe();
