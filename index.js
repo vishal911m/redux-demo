3debe09958555fe06e5e675da69b2f0417e66c14
vishal911m <vishal911m@gmail.com>
2026-02-23 23:21:25 +0530

    Store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux');
+const createStore = redux.createStore
+
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
 function orderCake(){
@@ -23,3 +26,14 @@ const reducer = (state = initialState, action) => {
       return state
   }
 }
+
+const store = createStore(reducer);
+console.log('Initial State', store.getState());
+
+const unsubscribe = store.subscribe(()=>console.log('Updated state', store.getState()));
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
