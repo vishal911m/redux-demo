2b4467a497a2d59a4ec210278b227620dc8c3c69
vishal911m <vishal911m@gmail.com>
2026-02-26 22:25:58 +0530

    Middleware

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const redux = require('redux');
 const createStore = redux.createStore
 const bindActionCreators = redux.bindActionCreators
 const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+
+const reduxLogger = require('redux-logger');
+const logger = reduxLogger.createLogger()
 
 const CAKE_ORDERED = 'CAKE_ORDERED';
 const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
@@ -90,10 +94,10 @@ const rootReducer = combineReducers({
   iceCream: iceCreamReducer
 })
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('Initial State', store.getState());
 
-const unsubscribe = store.subscribe(()=>console.log('Updated state', store.getState()));
+const unsubscribe = store.subscribe(()=>{});
 
 // store.dispatch(orderCake());
 // store.dispatch(orderCake());
