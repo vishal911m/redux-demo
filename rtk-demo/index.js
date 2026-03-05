3e8ebc55d5669ab4acc6e51ff4b34529d2e3a505
vishal911m <vishal911m@gmail.com>
2026-03-05 22:53:19 +0530

    Ice Cream Feature

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,5 +1,6 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
 
 console.log('Initial state ', store.getState())
 const  unsubscribe = store.subscribe(()=>{
@@ -10,5 +11,9 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
+
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(2));
 
 unsubscribe();
