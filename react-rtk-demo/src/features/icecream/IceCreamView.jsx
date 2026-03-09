401cf7d602d41232b764771a53e628312af39af1
vishal911m <vishal911m@gmail.com>
2026-03-09 11:51:51 +0530

    useDispatch

--- react-rtk-demo/src/features/icecream/IceCreamView.jsx
+++ react-rtk-demo/src/features/icecream/IceCreamView.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { ordered, restocked } from './icecreamSlice'
 
 export const IcecreamView = () => {
+  const [value, setValue] = useState(1)
   const numOfIcecreams =  useSelector((state) => state.icecream.numOfIcecreams)
+  const dispatch = useDispatch()
   return (
     <div>
       <h2>Number of ice creams - {numOfIcecreams}</h2>
-      <button>Order ice cream</button>
-      <button>Restock ice cream</button>
+      <button onClick={()=>dispatch(ordered())}>Order ice cream</button>
+      <input type="number" value={value} onChange={e => setValue(parseInt(e.target.value))} />
+      <button onClick={()=>dispatch(restocked(value))}>Restock ice cream</button>
     </div>
   )
 }
