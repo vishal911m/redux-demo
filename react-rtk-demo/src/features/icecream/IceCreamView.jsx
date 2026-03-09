0d45d1b42bd5a1562f99a1323f2379e7715f2e7a
vishal911m <vishal911m@gmail.com>
2026-03-09 11:39:25 +0530

    useSelector

--- react-rtk-demo/src/features/icecream/IceCreamView.jsx
+++ react-rtk-demo/src/features/icecream/IceCreamView.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const IcecreamView = () => {
+  const numOfIcecreams =  useSelector((state) => state.icecream.numOfIcecreams)
   return (
     <div>
-      <h2>Number of ice creams - </h2>
+      <h2>Number of ice creams - {numOfIcecreams}</h2>
       <button>Order ice cream</button>
       <button>Restock ice cream</button>
     </div>
